c9c8940f7598a17250bbb36b4d76e55a54bbe272
Aquarius1722 <taiyeoyedeji984@gmail.com>
2026-07-17 20:29:19 -0400

    Add console log for script file loading

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("Script file loaded!");
 // Smooth scroll for navigation links
 document.querySelectorAll("nav ul li a").forEach(link => {
   link.addEventListener("click", function(e) {
